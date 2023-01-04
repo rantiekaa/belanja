42378fe6cccb5881fc6991df7fbabece87bff3bf
rantiekaa <rantieka67@gmail.com>
2023-01-04 19:30:36 +0700

    update banner

--- application/controllers/Frontend.php
+++ application/controllers/Frontend.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Frontend extends CI_Controller {
 	public function index(){
         $data = array(
-            'title' => 'Belanja - Home'
+            'title' => 'Belanja - Home',
+            'listProduct' => $this->Backend_product->selectAll()
         );
         $this->template->load('frontend/template','frontend/index', $data);
 	}
